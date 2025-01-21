fbefbd8fc1ab90af2237d6a14d9b8743849efe43
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-01-21 21:47:41 +0200

    GET list

---
+++ src/main/java/com/marcelocaleb/dslist/repositories/GameListRepository.java
@@ -0,0 +1,9 @@
+package com.marcelocaleb.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcelocaleb.dslist.domain.GameList;
+
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
