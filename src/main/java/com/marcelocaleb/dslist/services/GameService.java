6438b4081e9dac517c59188fa556eb054042beaf
Marcelo Caleb Julio Ofice <marcelocalebjulioo@gmail.com>
2025-01-21 21:01:33 +0200

    Get game by id

--- src/main/java/com/marcelocaleb/dslist/services/GameService.java
+++ src/main/java/com/marcelocaleb/dslist/services/GameService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.marcelocaleb.dslist.domain.Game;
+import com.marcelocaleb.dslist.dto.GameDTO;
 import com.marcelocaleb.dslist.dto.GameMinDTO;
 import com.marcelocaleb.dslist.repositories.GameRepository;
 
@@ -15,6 +17,13 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
